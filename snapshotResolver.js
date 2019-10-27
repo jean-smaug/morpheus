5c44f3705ae00b68ebac3515858c1c4a2c88e51b
maxime blanc <maximeblanc.dev@gmail.com>
2019-10-27 14:29:21 +0100

    feat: morpheus snapshot is generated on root folder

---
+++ snapshotResolver.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  // resolves from test to snapshot path
+  resolveSnapshotPath: (testPath, snapshotExtension) => {
+    return testPath.replace("dist/", "") + snapshotExtension;
+  },
+  // resolves from snapshot to test path
+  resolveTestPath: (snapshotFilePath, snapshotExtension) => {
+    const splitedPath = snapshotFilePath.split("/");
+    const fileName = splitedPath[splitedPath.length - 1];
+
+    return snapshotFilePath
+      .slice(0, -fileName.length)
+      .concat("dist/")
+      .concat(fileName.slice(0, -snapshotExtension.length));
+  },
+
+  // Example test path, used for preflight consistency check of the implementation above
+  testPathForConsistencyCheck: `${__dirname}/dist/example.spec.js`
+};
