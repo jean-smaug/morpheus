a445e96acaa4298fa60786347179b2cf7590526f
jean-smaug <maximeblanc.dev@gmail.com>
2019-11-05 09:51:20 +0100

    refactor: start externalizing the the remplace template by variable method

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,27 @@
 import _get from "lodash/get";
 
+const VARIABLE = /{{\s*(?<variable>[\w.]+)\s*}}/i;
+
 export function replaceTemplateByValue(template: object, envs: object) {
 
+    const keys = Object.keys(template);
+    
+    keys.forEach(key => {
+      const execution = VARIABLE.exec(template[key]);
+      if (execution === null) {
+        return;
+      }
+  
+      if (!execution.groups) {
+        return;
+      }
+
+      
+      const { variable } = execution.groups;
+      const match = execution[0];
+  
+      template[key] = template[key].replace(match, _get(envs, variable));
+    });
+
+    return template;
 }
