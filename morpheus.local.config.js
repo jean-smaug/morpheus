7c00120f63be60d0c2a8148995706f8832747979
maxime blanc <maxime.blanc@deindeal.ch>
2019-11-19 12:06:18 +0100

    test: add local snapshot

---
+++ morpheus.local.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  verbose: true,
+  rootDir: __dirname,
+  snapshotResolver: "<rootDir>/snapshotResolver.js",
+  testMatch: ["<rootDir>/tests/morpheus.spec.js"],
+  testPathIgnorePatterns: []
+};
